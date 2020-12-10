47147c70f25b76175389b7d6b54253aa138dea50
jpadron1986 <40041071+jpadron1986@users.noreply.github.com>
2020-12-10 16:18:43 -0500

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":809},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-reportes","displayName":"reportes","link":{"linkType":"LinkTypePage","href":"#!page-reportes"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
